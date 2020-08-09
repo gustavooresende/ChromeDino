d51b0ce3b156ad916952a2002155b35b0ca56a68
gustavooresende <33909514+gustavooresende@users.noreply.github.com>
2020-08-09 19:45:03 -0300

    first commit

---
+++ dino.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+#define SERVO 6
+#define threshold 290
+#define pinLDR A0
+
+int count = 0;
+Servo s;
+int pos;
+
+void setup()
+{
+  pinMode(pinLDR, INPUT);
+
+  s.attach(SERVO);
+  s.write(0);
+  delay(3000);
+}
+void loop()
+{
+  // delay(1);
+  if (analogRead(pinLDR) > threshold)
+  {
+    count++;
+    s.write(30);
+    delay(100);
+    s.write(0);
+  }
+}
